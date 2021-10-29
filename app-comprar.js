eca75dd34fc9d3a59daa6ac643c50dcbce099933
Maxischiavello <maxischiavello12@gmail.com>
2021-10-29 13:57:41 -0300

    TP terminado

--- app-comprar.js
+++ app-comprar.js
@@ -2,21 +2,16 @@ const precio = 200;
 const descuentoEstudiante = 0.80;
 const descuentoTrainee = 0.50;
 const descuentoJunior = 0.15;
-const container = document.getElementById("seccion");
-const btnBorrar = document.getElementById("btn-borrar");
 const btnResumen = document.getElementById("btn-resumen");
-const totalOuput = document.getElementById("total")
+const totalOuput = document.getElementById("total");
 let total = 0;
 
-btnResumen.addEventListener('click', (event)=>{
+btnResumen.addEventListener('click', (event) => {
     event.preventDefault();
+    resumen();
 });
 
-btnBorrar.addEventListener('click', (event)=>{
-    window.location.reload();
-})
-
-const resumen = (event) => {
+const resumen = () => {
     total = generarResumen();
     totalOuput.innerHTML = `${total}`
 }
@@ -24,8 +19,9 @@ const resumen = (event) => {
 function generarResumen() {
     let categoria = document.getElementById("input-categoria");
     let cantidad = parseFloat(document.getElementById("input-cantidad").value);
+
     if (categoria.value == 'estudiante') {
-        let descuentoAplicado = precio -(precio * descuentoEstudiante);
+        let descuentoAplicado = precio - (precio * descuentoEstudiante);
         total = descuentoAplicado * cantidad;
     } else if (categoria.value == 'trainee') {
         let descuentoAplicado = precio - (precio * descuentoTrainee);
@@ -34,6 +30,7 @@ function generarResumen() {
         let descuentoAplicado = precio - (precio * descuentoJunior);
         total = descuentoAplicado * cantidad;
     }
+
     return total;
 }
 
